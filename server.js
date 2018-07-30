62a8fb6d1fe2c14c739ef12466e9081dae2c7a48
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-07-30 16:16:03 -0500

    update

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-console.log(__dirname);
-app.use(express.static(__dirname + '/dist'));
+console.log("----------------dirname", __dirname);
+console.log("----------------dirname", './dist');
+app.use(express.static('./dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
