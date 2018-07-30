180417d468e7f132398a17ed65f7290627f8823b
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-07-30 16:23:50 -0500

    pruebaupdate

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-// server.js
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-console.log("----------------dirname", __dirname);
-console.log("----------------dirname", './dist');
-app.use(express.static('./dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+app.use(bodyParser.json());
+
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
